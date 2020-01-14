c3cb457fb66871a2c2fb9aef4712aec80f1f0897
Tushar Malhotra <tusharmalhotra494@gmail.com>
2020-01-14 01:08:28 -0800

    commitin program number 38

---
+++ program_38.cpp
@@ -0,0 +1,18 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+	clrscr() ;
+
+	int a[5], i ;
+
+	cout<<"Enter values into array:\t" ;
+		for(i=0; i<n i++)
+			cin>>a[i] ;
+
+	cout<<"values are:\t" ;
+	for(i=0; i<n ;i++)
+	cout<<a[i] ;
+
+	getch() ;
+}
